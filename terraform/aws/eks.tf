0435842b3ebba74641ca22c90a64381ae22464f0
hexbalademo <BalaK1@hexaware.com>
2025-05-01 22:42:39 -0700

    approved

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -23,9 +23,9 @@ resource aws_iam_role "iam_for_eks" {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/eks.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
+    git_last_modified_by = "demo@demo.com"
     git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
+    git_org              = "Hexbala"
     git_repo             = "terragoat"
     yor_trace            = "de052596-21a0-43de-8153-469add277b18"
   }
